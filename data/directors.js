a2ac7f84392cab533250e22ce1b0762b485fe4c2
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-05-10 22:05:39 +0200

    Updates
    Hello Leonardo,
    It looks amazing. You tried very well. please note the below points.
    
    1) /genres/:name This route looks like you are getting Genre by name but you are getting movies by genre so the path should explains the working e.g.
    movies/genre/:genreName
    
    2) Error message you are returning string. better return JSON like below.
    let response = {"message" : "movies not found"}
    res.status(404).send(response);
    
    3) Mock data of movies, users and directors are taking much space so better to move to separate file and import and user.

---
+++ data/directors.js
@@ -0,0 +1,64 @@
+let directors = [
+    {
+      name: 'Christopher Nolan',
+      bio: 'Christopher Edward Nolan is a British-American film director, producer, and screenwriter.',
+      birthYear: 1970,
+      deathYear: null
+    },
+    {
+      name: 'Quentin Tarantino',
+      bio: 'Quentin Jerome Tarantino is an American film director, screenwriter, producer, and actor.',
+      birthYear: 1963,
+      deathYear: null
+    },
+    {
+      name: 'Steven Spielberg',
+      bio: 'Steven Allan Spielberg is an American film director, producer, and screenwriter.',
+      birthYear: 1946,
+      deathYear: null
+    },
+    {
+      name: 'Martin Scorsese',
+      bio: 'Martin Charles Scorsese is an American film director, producer, screenwriter, and actor.',
+      birthYear: 1942,
+      deathYear: null
+    },
+    {
+      name: 'Francis Ford Coppola',
+      bio: 'Francis Ford Coppola is an American film director, producer, and screenwriter.',
+      birthYear: 1939,
+      deathYear: null
+    },
+    {
+      name: 'Peter Jackson',
+      bio: 'Sir Peter Robert Jackson is a New Zealand film director, producer, and screenwriter.',
+      birthYear: 1961,
+      deathYear: null
+    },
+    {
+      name: 'David Fincher',
+      bio: 'David Andrew Leo Fincher is an American film director, producer, and screenwriter.',
+      birthYear: 1962,
+      deathYear: null
+    },
+    {
+      name: 'Robert Zemeckis',
+      bio: 'Robert Lee Zemeckis is an American film director, producer, and screenwriter.',
+      birthYear: 1951,
+      deathYear: null
+    },
+    {
+      name: 'Lana Wachowski',
+      bio: 'Lana Wachowski is an American film and television director, producer, and screenwriter.',
+      birthYear: 1965,
+      deathYear: null
+    },
+    {
+      name: 'Lilly Wachowski',
+      bio: 'Lilly Wachowski is an American film and television director, producer, and screenwriter.',
+      birthYear: 1967,
+      deathYear: null
+    },
+  ];
+
+  module.exports = directors;
--- index.js
+++ index.js
@@ -2,173 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const port = 8080;
-
-let topMovies = [
-  {
-    title: 'The Shawshank Redemption',
-    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    genre: 'Drama',
-    director: 'Frank Darabont',
-    imageURL: 'https://example.com/shawshank-redemption.jpg',
-    featured: true
-  },
-  {
-    title: 'The Godfather',
-    description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-    genre: 'Crime, Drama',
-    director: 'Francis Ford Coppola',
-    imageURL: 'https://example.com/the-godfather.jpg',
-    featured: false
-  },
-  {
-    title: 'Pulp Fiction',
-    description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
-    genre: 'Crime, Drama',
-    director: 'Quentin Tarantino',
-    imageURL: 'https://example.com/pulp-fiction.jpg',
-    featured: true
-  },
-  {
-    title: 'The Dark Knight',
-    description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-    genre: 'Action, Crime, Drama',
-    director: 'Christopher Nolan',
-    imageURL: 'https://example.com/the-dark-knight.jpg',
-    featured: false
-  },
-  {
-    title: 'Forrest Gump',
-    description: 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.',
-    genre: 'Drama, Romance',
-    director: 'Robert Zemeckis',
-    imageURL: 'https://example.com/forrest-gump.jpg',
-    featured: true
-  },
-  {
-    title: 'Inception',
-    description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-    genre: 'Action, Adventure, Sci-Fi',
-    director: 'Christopher Nolan',
-    imageURL: 'https://example.com/inception.jpg',
-    featured: false
-  },
-  {
-    title: 'Fight Club',
-    description: 'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into something much, much more.',
-    genre: 'Drama',
-    director: 'David Fincher',
-    imageURL: 'https://example.com/fight-club.jpg',
-    featured: true
-  },
-  {
-    title: 'The Matrix',
-    description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
-    genre: 'Action, Sci-Fi',
-    director: 'Lana Wachowski, Lilly Wachowski',
-    imageURL: 'https://example.com/the-matrix.jpg',
-    featured: false
-  },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-    genre: 'Action, Adventure, Drama',
-    director: 'Peter Jackson',
-    imageURL: 'https://example.com/the-fellowship-of-the-ring.jpg',
-    featured: true
-},
-{
-    title: 'Interstellar',
-    description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
-    genre: 'Adventure, Drama, Sci-Fi',
-    director: 'Christopher Nolan',
-    imageURL: 'https://example.com/interstellar.jpg',
-    featured: false
-}
-];
-
-let users = [
-  {
-    username: 'john_doe',
-    email: 'john_doe@example.com',
-    password: 'password123',
-    favoriteMovies: []
-  },
-  {
-    username: 'jane_doe',
-    email: 'jane_doe@example.com',
-    password: 'password456',
-    favoriteMovies: []
-  },
-  {
-    username: 'movie_fan',
-    email: 'movie_fan@example.com',
-    password: 'password789',
-    favoriteMovies: []
-  }
-];
-
-let directors = [
-  {
-    name: 'Christopher Nolan',
-    bio: 'Christopher Edward Nolan is a British-American film director, producer, and screenwriter.',
-    birthYear: 1970,
-    deathYear: null
-  },
-  {
-    name: 'Quentin Tarantino',
-    bio: 'Quentin Jerome Tarantino is an American film director, screenwriter, producer, and actor.',
-    birthYear: 1963,
-    deathYear: null
-  },
-  {
-    name: 'Steven Spielberg',
-    bio: 'Steven Allan Spielberg is an American film director, producer, and screenwriter.',
-    birthYear: 1946,
-    deathYear: null
-  },
-  {
-    name: 'Martin Scorsese',
-    bio: 'Martin Charles Scorsese is an American film director, producer, screenwriter, and actor.',
-    birthYear: 1942,
-    deathYear: null
-  },
-  {
-    name: 'Francis Ford Coppola',
-    bio: 'Francis Ford Coppola is an American film director, producer, and screenwriter.',
-    birthYear: 1939,
-    deathYear: null
-  },
-  {
-    name: 'Peter Jackson',
-    bio: 'Sir Peter Robert Jackson is a New Zealand film director, producer, and screenwriter.',
-    birthYear: 1961,
-    deathYear: null
-  },
-  {
-    name: 'David Fincher',
-    bio: 'David Andrew Leo Fincher is an American film director, producer, and screenwriter.',
-    birthYear: 1962,
-    deathYear: null
-  },
-  {
-    name: 'Robert Zemeckis',
-    bio: 'Robert Lee Zemeckis is an American film director, producer, and screenwriter.',
-    birthYear: 1951,
-    deathYear: null
-  },
-  {
-    name: 'Lana Wachowski',
-    bio: 'Lana Wachowski is an American film and television director, producer, and screenwriter.',
-    birthYear: 1965,
-    deathYear: null
-  },
-  {
-    name: 'Lilly Wachowski',
-    bio: 'Lilly Wachowski is an American film and television director, producer, and screenwriter.',
-    birthYear: 1967,
-    deathYear: null
-  },
-];
+const movies = require('./data/movies');
+const users = require('./data/users');
+const directors = require('./data/directors');
 
 function validateUserData(user) {
   const { username, email, password } = user;
@@ -199,11 +35,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
 app.get('/movies/:title', (req, res) => {
-  const movie = topMovies.find(m => m.title.toLowerCase() === req.params.title.toLowerCase());
+  const movie = movies.find(m => m.title.toLowerCase() === req.params.title.toLowerCase());
   if (movie) {
     res.json(movie);
   } else {
@@ -211,9 +47,9 @@ app.get('/movies/:title', (req, res) => {
   }
 });
 
-app.get('/genres/:name', (req, res) => {
-  const genreName = req.params.name.toLowerCase();
-  const genreMovies = topMovies.filter(movie => movie.genre.toLowerCase().includes(genreName));
+app.get('/genres/:genreName', (req, res) => {
+  const genreName = req.params.genreName.toLowerCase();
+  const genreMovies = movies.filter(movie => movie.genre.toLowerCase().includes(genreName));
 
   if (genreMovies.length > 0) {
     const response = {
@@ -223,10 +59,10 @@ app.get('/genres/:name', (req, res) => {
     };
     res.json(response);
   } else {
-    res.status(404).send('Genre not found');
+    let errorResponse = {"message" : "Genre not found"};
+    res.status(404).send(errorResponse);
   }
 });
-
 
 app.get('/directors/:name', (req, res) => {
   const directorName = req.params.name.toLowerCase();
@@ -248,9 +84,11 @@ app.post('/users', (req, res) => {
     return;
   }
 
+  newUser.favoriteMovies = [];
   users.push(newUser);
   res.status(201).json(newUser);
 });
+
 
 
 app.put('/users/:username', (req, res) => {
@@ -274,11 +112,12 @@ app.put('/users/:username', (req, res) => {
 
 
 app.post('/users/:username/favorites/:movieTitle', (req, res) => {
+  console.log('Adding movie to favorites');
   const username = req.params.username;
   const movieTitle = req.params.movieTitle;
   
   const user = users.find(u => u.username.toLowerCase() === username.toLowerCase());
-  const movie = topMovies.find(m => m.title.toLowerCase() === movieTitle.toLowerCase());
+  const movie = movies.find(m => m.title.toLowerCase() === movieTitle.toLowerCase());
 
   if (!user) {
     res.status(404).send('User not found');
@@ -295,26 +134,6 @@ app.post('/users/:username/favorites/:movieTitle', (req, res) => {
   }
 });
 
-app.delete('/users/:username/favorites/:movieTitle', (req, res) => {
-  const username = req.params.username;
-  const movieTitle = req.params.movieTitle;
-
-  const user = users.find(u => u.username === username);
-
-  if (!user) {
-    res.status(404).send('User not found');
-  } else {
-    const movieIndex = user.favoriteMovies.findIndex(m => m.title.toLowerCase() === movieTitle.toLowerCase());
-
-    if (movieIndex === -1) {
-      res.status(404).send('Movie not found in favorites');
-    } else {
-      user.favoriteMovies.splice(movieIndex, 1);
-      res.status(200).send('Movie removed from favorites');
-    }
-  }
-});
-
 app.delete('/users/:email', (req, res) => {
   const index = users.findIndex(u => u.email === req.params.email);
   if (index !== -1) {
@@ -325,6 +144,27 @@ app.delete('/users/:email', (req, res) => {
   }
 });
 
+app.delete('/users/:username/favorites/:movieTitle', (req, res) => {
+  const username = req.params.username;
+  const movieTitle = req.params.movieTitle;
+
+  const user = users.find(u => u.username.toLowerCase() === username.toLowerCase());
+  const movie = movies.find(m => m.title.toLowerCase() === movieTitle.toLowerCase());
+
+  if (!user) {
+    res.status(404).send('User not found');
+  } else if (!movie) {
+    res.status(404).send('Movie not found');
+  } else {
+    const movieIndex = user.favoriteMovies.findIndex(favMovie => favMovie.title.toLowerCase() === movieTitle.toLowerCase());
+    if (movieIndex === -1) {
+      res.status(404).send('Movie not in the favorites list');
+    } else {
+      user.favoriteMovies.splice(movieIndex, 1);
+      res.status(200).send('Movie removed from favorites');
+    }
+  }
+});
 
 
 app.use(express.static('public'));
@@ -332,13 +172,9 @@ app.use(morgan('combined'));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(err.status || 500).send({ message: err.message || 'Something went wrong!' });
 });
 
-app.get('/error', (req, res, next) => {
-  const error = new Error('This is a test error');
-  next(error);
-});
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
