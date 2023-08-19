cac13446f7a31da89b0b82a75032ca0b35037355
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-08-19 11:38:55 +0200

    udate?

--- index.js
+++ index.js
@@ -32,6 +32,7 @@
   //Middleware
   app.use(cors());
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   // Middleware per loggare le richieste
   app.use((req, res, next) => {
